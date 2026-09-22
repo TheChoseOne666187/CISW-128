de44a1cb5854eaa03915f3920606ee925c72d1d1
TheChoseOne666187 <namcmanus@csi.edu>
2026-09-22 10:10:01 -0600

    update

---
+++ Week5/LastNameHW5/scripts.js
@@ -0,0 +1,16 @@
+        console.log("MyJS");
+        let Me = "MAC";      
+        const age = 47;
+        console.log(Me);  
+        console.log(age);
+        const isAdult=true; 
+        console.log(isAdult);
+
+        let first="MyJS";
+        let second="WEB APP";
+        console.log(first + " " + second); 
+        console.log(1+1+1*10); 
+        console.log(1+1+1*10/2); 
+        console.log(1+1+1*10/2-5); 
+        document.body.innerHTML += "<p>Result: " + total + "</p>";
+    
