3a8913c572ffc38fbe8ba89188ec4c265744cedb
TheChoseOne666187 <nmacmanus@csi.edu>
2026-09-23 11:03:02 -0600

    week 6 lab update

---
+++ Week6 LAB/scripts.js
@@ -0,0 +1,63 @@
+// // comparison operator
+// // === equal to
+// // !== not equal
+
+// console.log(1==1); // true
+// console.log(1==2); // false
+
+// let score = 100;
+// if(score === 100){
+//     console.log("You got a perfect score!");
+// }
+
+// else if(score !== 100){
+//     console.log("You did not get a perfect score.");
+// }
+// else if(score > 100){
+//     console.log("You got more than a perfect score!");
+// }       
+
+// else{ console.log("You did not get a perfect score.");
+// }
+
+
+let age = 18;
+let isMember = true;
+if (age >= 18) {
+    if (isMember) {
+        console.log("You are an adult and a member.");
+    } else {
+        console.log("You are an adult but not a member.");
+    }
+} else {
+    console.log("You are not an adult.");
+}
+
+
+// We use Prompt() to make our websites more interactive
+// this variable is hardcode, user cant change this
+
+
+// if (score >= 90) {
+//     console.log("You got an A!");
+// }else if (score >= 80) {
+//     console.log("You got a B!");
+// } else if (score >= 70) {
+//     console.log("You got a C!");
+// }else if (score >= 60) {
+//     console.log("You got a D!");
+// }else {
+//     console.log("You got an F!");
+// }  
+
+// let temperature = Number(prompt("Enter the temperature in Fahrenheit: "));
+// if (temperature >= 212) {
+//     console.log("The water is boiling.");
+// }
+
+
+// document.body.innerHTML = "<h1> Your score is: " + score + "</h1>";
+//  i want the website to ask for the username and display it on the page "hello"
+
+// let username = prompt("Enter your username: ");
+// document.body.innerHTML = "<h1>Hello, " + username + "!</h1>";
